e847baf541a4925a66eaea937cfb8e4985627497
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-02 17:12:21 +0700

    environment(homeserver): add flaresolverr service

--- environments/homeserver/system/default.nix
+++ environments/homeserver/system/default.nix
@@ -3,6 +3,7 @@
     ./adguard_home.nix
     ./anubis.nix
     ./caddy.nix
+    ./flaresolverr.nix
     ./pocket_id.nix
     ./tiny_auth.nix
   ];
---
+++ environments/homeserver/system/flaresolverr.nix
@@ -0,0 +1,27 @@
+{ config, ... }:
+{
+  services.flaresolverr.enable = true;
+  systemd.socketActivations.flaresolverr = {
+    host = "127.0.0.1";
+    port = config.services.flaresolverr.port;
+    idleTimeout = "30s";
+  };
+  systemd.services.flaresolverr = {
+    # Flaresolverr takes huge amount of resources because chromium.
+    #
+    # This is a workaround to limit the resources it can use.
+    serviceConfig = {
+      CPUWeight = 10;
+      CPUQuota = "10%";
+      MemoryMax = "2G";
+    };
+    environment = {
+      HOST = "127.0.0.1";
+    };
+  };
+  services.caddy.virtualHosts."http://flaresolverr.local".extraConfig =
+    # caddy
+    ''
+      reverse_prxy unix/${config.systemd.socketActivations.flaresolverr.address}
+    '';
+}
