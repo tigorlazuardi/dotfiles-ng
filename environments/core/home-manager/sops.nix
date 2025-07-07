5662a42e40bbfcff4cc27f82b3a9d371ada1c55e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-07 21:37:34 +0700

    revert sops config

--- environments/core/home-manager/sops.nix
+++ environments/core/home-manager/sops.nix
@@ -12,7 +12,5 @@
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     defaultSopsFormat = "yaml";
-    defaultSymlinkPath = "/run/user/1000/secrets";
-    defaultSecretsMountPoint = "/run/user/1000/secrets.d";
   };
 }
