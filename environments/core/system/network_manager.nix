3ce253bca02b9e2ff12e102a9912e36e25c38013
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-11 00:50:25 +0700

    network-manager: use insertNameservers instead of appendNameservers

--- environments/core/system/network_manager.nix
+++ environments/core/system/network_manager.nix
@@ -6,7 +6,7 @@
 }:
 {
   networking.networkmanager.enable = true;
-  networking.networkmanager.appendNameservers = lib.optional config.services.adguardhome.enable "192.168.100.5";
+  networking.networkmanager.insertNameservers = lib.optional config.services.adguardhome.enable "192.168.100.5";
   users.users.${user.name}.extraGroups = [
     "networkmanager"
   ];
