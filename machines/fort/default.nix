5ca813605817c0909de33446ebd872cb3cb1f51d
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-11 10:58:30 +0700

    fort: import environments

--- machines/fort/default.nix
+++ machines/fort/default.nix
@@ -1,10 +1,26 @@
+{ user, ... }:
 {
   imports = [
     ../../environments/core/system
+    ../../environments/bareksa/system
+
+    # We will use KDE Plasma 6 as the desktop environment
+    ../../environments/kde/system
+    ../../environments/game/system
 
     ./hardware.nix
     ./system.nix
   ];
+
+  home-manager.users.${user.name} = {
+    imports = [
+      ../../environments/core/home-manager
+      ../../environments/bareksa/home-manager
+      ../../environments/desktop/home-manager
+      ../../environments/nixvim/home-manager
+      ../../environments/ai/home-manager
+    ];
+  };
 
   networking.hostName = "fort";
   system.stateVersion = "23.11";
