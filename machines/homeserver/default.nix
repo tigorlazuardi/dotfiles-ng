8f85c9d46571627dc1ababbeef4053f556b101d3
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-14 12:35:15 +0700

    homeserver: Fix authorized_keys file permissions and ownership

--- machines/homeserver/default.nix
+++ machines/homeserver/default.nix
@@ -17,10 +17,15 @@
   };
   networking.hostName = "homeserver";
   system.stateVersion = "24.05";
-  sops.secrets."ssh/homeserver/authorized_keys".sopsFile = ../../secrets/ssh.yaml;
+  sops.secrets."ssh/homeserver/authorized_keys" = {
+    sopsFile = ../../secrets/ssh.yaml;
+    path = "/home/${user.name}/.ssh/authorized_keys";
+    mode = "0600";
+    owner = user.name;
+  };
 
   # Allows ssh access to this homeserver user from the following machines.
-  users.users.${user.name}.openssh.authorizedKeys.keyFiles = [
-    config.sops.secrets."ssh/homeserver/authorized_keys".path
-  ];
+  # users.users.${user.name}.openssh.authorizedKeys.keyFiles = [
+  #   config.sops.secrets."ssh/homeserver/authorized_keys".path
+  # ];
 }
