3d4abd05358a92444f258c32c6df8ffaeeb93bd2
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-15 15:10:44 +0700

    refactor(discord): convert autostart from shell script to desktop file
    - Rename startScript to script and change script name to discord-autostart
    - Replace .config/autostart/discord.sh with proper .config/autostart/vesktop.desktop
    - Generate desktop file by modifying vesktop.desktop Exec line
    - Add argument support to script with "$@"

--- environments/desktop/home-manager/discord.nix
+++ environments/desktop/home-manager/discord.nix
@@ -1,14 +1,14 @@
 { pkgs, lib, ... }:
 let
   inherit (lib.meta) getExe;
-  startScript = # sh
-    ''
-      until ${pkgs.netcat}/bin/nc -z discord.com 443 > /dev/null; do
-        ${pkgs.coreutils}/bin/sleep 0.1
-      done
-      ${pkgs.vesktop}/bin/vesktop
-    '';
-  scriptFile = getExe (pkgs.writeShellScriptBin "discord.sh" startScript);
+  script =
+    pkgs.writeShellScriptBin "discord-autostart" # sh
+      ''
+        until ${pkgs.netcat}/bin/nc -z discord.com 443 > /dev/null; do
+          ${pkgs.coreutils}/bin/sleep 0.1
+        done
+        ${pkgs.vesktop}/bin/vesktop "$@"
+      '';
 in
 {
   home = {
@@ -17,10 +17,14 @@ in
       ".config/discord/settings.json".source = (pkgs.formats.json { }).generate "settings.json" {
         SKIP_HOST_UPDATE = true;
       };
-      ".config/autostart/discord.sh".source = scriptFile;
+      ".config/autostart/vesktop.desktop".source =
+        (pkgs.runCommand "discord-autostart" { }) # sh
+          ''
+            sed -e 's#Exec=.*#Exec=${getExe script} %U#' ${pkgs.vesktop}/share/applications/vesktop.desktop > $out
+          '';
     };
   };
-  wayland.windowManager.hyprland.settings.exec-once = [ scriptFile ];
+  wayland.windowManager.hyprland.settings.exec-once = [ script ];
   services.swaync.settings.scripts._10-discord = {
     app-name = "[Vv]esktop";
     exec = "hyprctl dispatch focuswindow vesktop";
