1cf229ad3a843263818a2b8ab78c907a5be16aed
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-15 14:58:47 +0700

    whatsapp: changed from wasistlos to whatsie

--- environments/desktop/home-manager/whatsapp.nix
+++ environments/desktop/home-manager/whatsapp.nix
@@ -5,21 +5,21 @@
 }:
 {
   home.packages = with pkgs; [
-    wasistlos
+    whatsie
   ];
 
   home.file.".config/autostart/whatsapp.desktop".source =
     let
       script = pkgs.writeShellScriptBin "whatsapp-autostart" ''
-        until ${pkgs.netcat}/bin/nc -z web.whatsapp.com 443; do
+        until ${pkgs.netcat}/bin/nc -z web.whatsapp.com 443 > /dev/null; do
           sleep 0.1
         done
-        ${pkgs.wasistlos}/bin/wasistlos
+        ${pkgs.whatsie}/bin/whatsie
       '';
       inherit (lib.meta) getExe;
     in
     (pkgs.runCommand "whatsapp" { }) # sh
       ''
-        sed -e 's#Exec=.*#Exec=${getExe script}#' ${pkgs.wasistlos}/share/applications/com.github.xeco23.WasIstLos.desktop > $out
+        sed -e 's#Exec=.*#Exec=${getExe script}#' ${pkgs.whatsie}/share/applications/whatsie.desktop > $out
       '';
 }
