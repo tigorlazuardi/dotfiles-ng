ee37d88f38fba3e4e23dac92c482563279fbc116
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-19 13:27:06 +0700

    feat(desktop): added Feishin as an optional package

--- machines/castle/default.nix
+++ machines/castle/default.nix
@@ -24,7 +24,7 @@
       ../../environments/nixvim/home-manager
 
       ../../environments/desktop/home-manager/optional/obs-studio.nix
-      ../../environments/desktop/home-manager/optional/supersonic.nix
+      ../../environments/desktop/home-manager/optional/feishin.nix
 
       ./home-manager.nix
     ];
