ad1f838caf8cf0f45837e766e588fe440d42a1cf
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-22 15:42:24 +0700

    system: removd duplicate nix config

--- environments/core/system/nix_config.nix
+++ environments/core/system/nix_config.nix
@@ -1,8 +1,4 @@
 {
-  lib,
-  ...
-}:
-{
   nixpkgs.config.allowUnfree = true;
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
   nix.settings.experimental-features = [
@@ -13,11 +9,4 @@
     http-connections = 8
     connect-timeout = 5
   '';
-
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3 --nogcroots"; # --nogcroots prevents direnv caches from being deleted
-    clean.dates = "weekly";
-  };
 }
