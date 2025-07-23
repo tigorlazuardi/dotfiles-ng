71af495ef6e48cc960c0900aaecee6afbef9f3b3
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-23 16:32:48 +0700

    nix: update post build hook

--- environments/core/system/nix_config.nix
+++ environments/core/system/nix_config.nix
@@ -1,13 +1,14 @@
 {
   nixpkgs.config.allowUnfree = true;
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-  nix.settings.trusted-users = [ "@wheel" ];
-  nix.extraOptions = ''
-    http-connections = 8
-    connect-timeout = 5
-  '';
+  nix.settings = {
+    accept-flake-config = true;
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    trusted-users = [ "@wheel" ];
+    http-connections = 8;
+    connect-timeout = 5;
+  };
 }
--- environments/core/system/optional/post-build-hook.nix
+++ environments/core/system/optional/post-build-hook.nix
@@ -1,15 +1,16 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 let
   domain = "ssh-serve.tigor.web.id";
 in
 {
-  nix.settings.post-build-hook = lib.getExe (
-    pkgs.writeShellScriptBin "post-build-hook" ''
-      set -uf
-      export IFS=' '
-      echo "Uploading paths to ${domain}" $OUT_PATHS
-      ${pkgs.nix}/bin/nix copy --to "ssh://nix-ssh@${domain}" $OUT_PATHS || true
-      exit 0
-    ''
-  );
+  nix.settings.post-build-hook = pkgs.writeShellScriptBin "post-build-hook" ''
+    #!/bin/sh
+
+    set -eu
+    set -f # disable globbing
+    export IFS=' '
+
+    echo "Uploading paths" $OUT_PATHS
+    exec nix copy --to "ssh://${domain}" $OUT_PATHS
+  '';
 }
