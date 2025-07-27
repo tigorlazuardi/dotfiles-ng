48d960bc5e0d6408a3e32777b6494223b848e8e4
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-27 23:00:20 +0700

    feat(homeserver): update N8N Config

--- environments/homeserver/system/n8n.nix
+++ environments/homeserver/system/n8n.nix
@@ -12,6 +12,8 @@ in
     N8N_USER_MANAGEMENT_JWT_SECRET = "FUCK-SAML-LOGINS";
     N8N_USER_MANAGEMENT_JWT_DURATION_HOURS = toString (24 * 365 * 365 * 365);
     N8N_USER_MANAGEMENT_JWT_REFRESH_TIMEOUT_HOURS = toString (24 * 365 * 365 * 365);
+    N8N_RUNNERS_ENABLED = "true";
+    GENERIC_TIMEZONE = "Asia/Jakarta";
   };
   services.nginx.virtualHosts."${domain}" = {
     forceSSL = true;
