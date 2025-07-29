d813d8db57cbdeddb4d4052daa65be0ce9d12e61
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-29 12:47:33 +0700

    flake: fix extra-trusted-public-keys

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       "https://walker-git.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nix.tigor.web.id-1:18Jg7EtxhZX8fE+VYyxHNcJb8Faw4gFKV+QB47mWtOw="
+      "nix.tigor.web.id:18Jg7EtxhZX8fE+VYyxHNcJb8Faw4gFKV+QB47mWtOw="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
