af5e549240f57407785b83617ae0514db4dae38e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-31 22:47:31 +0700

    feat(kde): add plasma-manager home-manager configuration

--- machines/castle/default.nix
+++ machines/castle/default.nix
@@ -31,6 +31,8 @@
       ../../environments/desktop/home-manager/optional/feishin.nix
       ../../environments/desktop/home-manager/optional/ntfy_client.nix
 
+      ../../environments/kde/home-manager.nix
+
       ./home-manager.nix
     ];
   };
