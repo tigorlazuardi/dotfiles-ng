ad13604d08784323f0c8cc8458c570969aa89e82
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-08 23:01:13 +0700

    wip(hyprland): add greetd and regreet

--- environments/hyprland/system.nix
+++ environments/hyprland/system.nix
@@ -1,8 +1,15 @@
-{ pkgs, ... }:
 {
-  programs.hyprland.enable = true;
+  pkgs,
+  user,
+  config,
+  ...
+}:
+{
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
   services.gnome.gnome-keyring.enable = true;
-  services.displayManager.gdm.enable = true;
   programs.dconf.enable = true;
   xdg.mime.enable = true;
   xdg.icons.enable = true;
@@ -12,4 +19,28 @@
   services.avahi.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.hyprlock.enable = true;
+
+  services.greetd = {
+    enable = true;
+    restart = true;
+    settings = {
+      user = user.name;
+      command =
+        let
+          cfg =
+            pkgs.writeText "hyprland-greetd.conf" # hyprlang
+              ''
+                exec-once = ${config.programs.regreet.package}/bin/regreet; hyprctl dispatch exit
+                misc {
+                  disable_hyprland_logo = true
+                  disable_splash_rendering = true
+                }
+              '';
+          command = "${config.programs.hyprland.package}/bin/Hyprland --config ${cfg}";
+        in
+        command;
+    };
+  };
+  security.pam.services.greetd.enableGnomeKeyring = true;
+  programs.regreet.enable = true;
 }
