664428a6d9de3a007c9317093bf829e009344453
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-12 00:13:37 +0700

    feat(walker): add Hyprland windows plugin

--- environments/hyprland/home-manager/walker.nix
+++ environments/hyprland/home-manager/walker.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
 {
   imports = [
@@ -12,6 +17,60 @@ with lib;
     };
     Install.WantedBy = mkForce [
       config.wayland.systemd.target
+    ];
+  };
+  programs.walker.config = {
+    terminal = lib.mkForce (lib.meta.getExe' config.programs.foot.package "footclient");
+    plugins = [
+      (
+        let
+          python = lib.meta.getExe' pkgs.python3 "python";
+          srcScript =
+            pkgs.writeText "walker-hyprland-windows.py" # python
+              ''
+                import json
+                import subprocess
+
+                # Icon mappings for class names
+                icon_mappings = {
+                    "vivaldi-stable": "vivaldi",
+                    "footclient": "foot"
+                }
+
+                # Get Hyprland clients
+                result = subprocess.run(['hyprctl', 'clients', '-j'], capture_output=True, text=True)
+                clients = json.loads(result.stdout)
+
+                entries = []
+                for client in clients:
+                    class_name = client["class"]
+                    icon_name = icon_mappings.get(class_name, class_name)
+                    
+                    # Entry schema: https://github.com/abenz1267/walker/wiki/Plugins
+                    # Follow the `json` tag of Entry struct.
+                    entry = {
+                        "label": client["title"],
+                        "sub": f"Workspace {client['workspace']['name']} • {client['class']}",
+                        "class": client["class"],
+                        "initial_class": client["initialClass"],
+                        "exec": f"hyprctl dispatch focuswindow address:{client['address']}",
+                        "searchable": f"{client['title']} {client['class']} {client['initialClass']}",
+                        "icon": icon_name
+                    }
+                    entries.append(entry)
+
+                print(json.dumps(entries))
+              '';
+        in
+        {
+          name = "Hyprland Windows";
+          placeholder = "Hyprland";
+          show_icon_when_single = true;
+          src = "${python} ${srcScript}";
+          parser = "json";
+          weight = 100;
+        }
+      )
     ];
   };
   wayland.windowManager.hyprland.settings = {
