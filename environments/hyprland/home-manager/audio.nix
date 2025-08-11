1637fd5bb68aba0a9e1d5c63293d5050084f0e8c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-11 23:15:51 +0700

    feat(hyprland): removed pasystray from home.packages

--- environments/hyprland/home-manager/audio.nix
+++ environments/hyprland/home-manager/audio.nix
@@ -10,11 +10,12 @@ in
 {
   home.packages = with pkgs; [
     pavucontrol
-    pasystray
     pamixer
     playerctl
   ];
 
+  # Do not put pasystray in home.packages. It will install xdg autostart desktop file
+  # which is not what we want, because we cannot control it.
   systemd.user.services.pasystray = {
     Unit = {
       Description = "System Tray Panel for Pulse Audio";
