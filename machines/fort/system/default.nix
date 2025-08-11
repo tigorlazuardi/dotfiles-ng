08e0c6085f9a0ef31b014b02333c9cf59f7a86e8
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-11 18:40:05 +0700

    feat(fort): use tlp for power management

--- machines/fort/system/default.nix
+++ machines/fort/system/default.nix
@@ -47,15 +47,30 @@
     ];
   };
 
-  powerManagement = {
-    powertop.enable = true;
-    cpuFreqGovernor = "powersave";
-  };
-
   services.thermald.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
+    };
+  };
+  services.upower.enable = true;
 
   services.tuned = {
-    enable = true;
+    enable = false;
     settings.dynamic_tuning = true;
   };
 }
