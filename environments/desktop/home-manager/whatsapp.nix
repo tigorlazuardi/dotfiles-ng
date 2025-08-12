761b885f7fcca861f3b61081893429088971f529
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-12 22:36:11 +0700

    feat(whatsapp): use systemd service for autostart

--- environments/desktop/home-manager/whatsapp.nix
+++ environments/desktop/home-manager/whatsapp.nix
@@ -1,28 +1,32 @@
 {
+  config,
   pkgs,
   lib,
   ...
 }:
+let
+  script = pkgs.writeShellScriptBin "whatsapp-autostart" ''
+    until ${pkgs.netcat}/bin/nc -z web.whatsapp.com 443 > /dev/null; do
+      sleep 0.1
+    done
+    ${pkgs.wasistlos}/bin/wasistlos
+  '';
+  inherit (lib.meta) getExe;
+in
 {
   home.packages = with pkgs; [
     wasistlos
   ];
 
-  xdg.autostart.entries = [
-    (
-      let
-        script = pkgs.writeShellScriptBin "whatsapp-autostart" ''
-          until ${pkgs.netcat}/bin/nc -z web.whatsapp.com 443 > /dev/null; do
-            sleep 0.1
-          done
-          ${pkgs.wasistlos}/bin/wasistlos
-        '';
-        inherit (lib.meta) getExe;
-      in
-      (pkgs.runCommand "whatsapp.desktop" { }) # sh
-        ''
-          sed -e 's#Exec=.*#Exec=${getExe script}#' ${pkgs.wasistlos}/share/applications/com.github.xeco23.WasIstLos.desktop > $out
-        ''
-    )
-  ];
+  systemd.user.services.whatsapp-autostart = {
+    Unit = {
+      Description = "WhatsApp Autostart Service";
+      After = [ config.wayland.systemd.target ];
+      PartOf = [ config.wayland.systemd.target ];
+    };
+    Service = {
+      ExecStart = "${getExe script}";
+    };
+    Install.WantedBy = [ config.wayland.systemd.target ];
+  };
 }
