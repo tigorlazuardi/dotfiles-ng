a0e99aec859808d850dfb81683267fe362807e29
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-12 15:34:52 +0700

    feat(walker): audio select now uses Python script

--- environments/desktop/home-manager/walker.nix
+++ environments/desktop/home-manager/walker.nix
@@ -78,24 +78,54 @@
       plugins = [
         (
           let
+            python = lib.meta.getExe' pkgs.python3 "python";
             pactl = lib.meta.getExe' pkgs.pulseaudio "pactl";
-            jq = lib.meta.getExe pkgs.jq;
+            speakerIcon = pkgs.fetchurl {
+              url = "https://www.svgrepo.com/download/522673/speaker.svg";
+              hash = "sha256-XDytdJGb7WhI89NWZmYeaFr/cB1BYzPmwYkZjrPnKNE=";
+            };
+            earphoneIcon = pkgs.fetchurl {
+              url = "https://www.svgrepo.com/download/461261/earphone.svg";
+              hash = "sha256-onU/+VXQFUCKutNLqytp4M2fT3i877cPk2KujieALUI=";
+            };
+            srcScript =
+              pkgs.writeText "walker-audio.py" # python
+                ''
+                  import json
+                  import subprocess
+
+                  result = subprocess.run(['${pactl}', '-f', 'json', 'list', 'sinks'], capture_output=True, text=True)
+                  sinks = json.loads(result.stdout)
+
+                  icon_mappings = {
+                    "KM-HIFI-384KHZ Analog Stereo": "${earphoneIcon}",
+                  }
+
+                  entries = []
+                  for sink in sinks:
+                      icon = icon_mappings.get(sink["description"], "${speakerIcon}")
+                      # Entry schema: https://github.com/abenz1267/walker/wiki/Plugins
+                      # Follow the `json` tag of Entry struct.
+                      entry = {
+                          "label": sink["description"],
+                          "sub": f"State: {sink['state']} • {sink['name']}",
+                          "name": sink["name"],
+                          "description": sink["description"],
+                          "exec": f"${pactl} set-default-sink {sink['name']}",
+                          "searchable": f"{sink['description']} {sink['name']}",
+                          "icon": icon
+                      }
+                      entries.append(entry)
+
+                  print(json.dumps(entries))
+                '';
           in
           {
             name = "audio";
             placeholder = "Select Audio Output";
             show_icon_when_single = true;
-            # TODO: change this to Python script so better listing of audio sinks
-            src = # sh
-              "${pactl} -f json list sinks | ${jq} -r '.[].description'";
-            cmd = # sh
-              ''${lib.meta.getExe (
-                pkgs.writeShellScriptBin "walker-audio" ''
-                  selected=$(${pactl} -f json list sinks | ${jq} --arg result "$1" -r '.[] | select(.description == $result) | .name')
-                  echo "$selected"
-                  ${pactl} set-default-sink "$selected"
-                ''
-              )} "%RESULT%"'';
+            src = "${python} ${srcScript}";
+            parser = "json";
           }
         )
       ];
