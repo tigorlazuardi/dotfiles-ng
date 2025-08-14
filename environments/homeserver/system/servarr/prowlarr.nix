c1e540285ede1428ff2ef143ef39018d9b912001
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-14 14:00:13 +0700

    feat(prowlarr): now in subpath /prowlarr instead of root to reduce ACME certificate requests

--- environments/homeserver/system/servarr/prowlarr.nix
+++ environments/homeserver/system/servarr/prowlarr.nix
@@ -4,7 +4,7 @@ let
   configVolume = "${root}/prowlarr";
   inherit (config.users.users.servarr) uid;
   inherit (config.users.groups.servarr) gid;
-  domain = "prowlarr.tigor.web.id";
+  # domain = "prowlarr.tigor.web.id";
 in
 {
   sops = {
@@ -27,7 +27,7 @@ in
             <SslCertPassword></SslCertPassword>
             <SslCertPath></SslCertPath>
             <UpdateMechanism>Docker</UpdateMechanism>
-            <UrlBase></UrlBase>
+            <UrlBase>/prowlarr</UrlBase>
           </Config>
         '';
     };
@@ -54,24 +54,24 @@ in
   services.nginx.virtualHosts =
     let
       inherit (config.virtualisation.oci-containers.containers.prowlarr) ip httpPort;
-      proxyPass = "http://${ip}:${toString httpPort}";
+      proxyPass = "http://${ip}:${toString httpPort}/prowlarr";
     in
     {
-      "${domain}" = {
+      "tigor.web.id" = {
         forceSSL = true;
-        tinyauth.locations = [ "/" ];
-        locations."/".proxyPass = proxyPass;
+        tinyauth.locations = [ "/prowlarr" ];
+        locations."/prowlarr".proxyPass = proxyPass;
       };
       "prowlarr.lan".locations."/".proxyPass = proxyPass;
     };
   services.homepage-dashboard.groups."Media Collectors".services.Prowlarr.settings = {
     description = "Indexer manager for the servarr stack";
-    href = "https://${domain}";
+    href = "https://tigor.web.id/prowlarr";
     icon = "prowlarr.svg";
     user = "${toString uid}:${toString gid}";
     widget = {
       type = "prowlarr";
-      url = "http://prowlarr.lan";
+      url = "http://prowlarr.lan/prowlarr";
       key = "{{HOMEPAGE_VAR_PROWLARR_API_KEY}}";
     };
   };
