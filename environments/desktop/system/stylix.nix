3a903c231fb3292a80f84a4418c0e52de9b2bd9a
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-16 00:23:32 +0700

    feat(stylix): added transparency support

--- environments/desktop/system/stylix.nix
+++ environments/desktop/system/stylix.nix
@@ -7,5 +7,11 @@
     enable = true;
     image = null;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    opacity = {
+      applications = 0.6;
+      desktop = 0.9;
+      popups = 0.6;
+      terminal = 0.9;
+    };
   };
 }
