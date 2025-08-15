1d9d9d75c360e45ed84dec3050a5a9013ee3d0dc
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-15 09:31:57 +0700

    feat(hyprshot): swap keybindings for screenshot actions
    Now, the Print key captures region screenshots by default and send it to
    satty, while SUPER + Print captures window screenshots and sends them to satty.
    
    Adding Shift will not send the screenshot to satty, but save it directly to the
    screenshot directory.

--- environments/hyprland/home-manager/hyprshot.nix
+++ environments/hyprland/home-manager/hyprshot.nix
@@ -11,10 +11,10 @@ let
 in
 {
   wayland.windowManager.hyprland.settings.bind = [
-    "SUPER, Print, exec, ${hyprshot} -m region -o ${screenshotDir}"
-    "SHIFT_SUPER, Print, exec, ${hyprshot} -m region --raw --silent | ${satty} --filename - --output-filename ${screenshotDir}/screenshot-$(date +%Y-%m-%d_%H-%M-%S).png"
-    ", Print, exec, ${hyprshot} -m window --raw --silent | ${satty} --filename - --output-filename ${screenshotDir}/screenshot-$(date +%Y-%m-%d_%H-%M-%S).png"
-    "SHIFT, Print, exec, ${hyprshot} -m window -o ${screenshotDir}"
+    ", Print, exec, ${hyprshot} -m region --raw --silent | ${satty} --filename - --output-filename ${screenshotDir}/screenshot-$(date +%Y-%m-%d_%H-%M-%S).png"
+    "SHIFT, Print, exec, ${hyprshot} -m region -o ${screenshotDir}"
+    "SUPER, Print, exec, ${hyprshot} -m window --raw --silent | ${satty} --filename - --output-filename ${screenshotDir}/screenshot-$(date +%Y-%m-%d_%H-%M-%S).png"
+    "SHIFT_SUPER, Print, exec, ${hyprshot} -m windo -o ${screenshotDir}"
   ];
   systemd.user.services.create-screenshot-dir = {
     Unit.Description = "Create screenshot directory";
