244869e545e3e493b1b649181c79794bf7fd0639
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-16 16:15:30 +0700

    feat(wpaperd): wallpaper script only runs on main monitors

--- environments/window-manager/home-manager/wpaperd.nix
+++ environments/window-manager/home-manager/wpaperd.nix
@@ -6,31 +6,40 @@
 }:
 let
   dataDir = "${config.xdg.dataHome}/wallpapers";
+  postWallpaperScript = "${pkgs.writeShellScript "post-wallpaper-change" # sh
+    ''
+      wallpaper=$2
+
+      set -e
+
+      ln -sfn "$wallpaper" ${dataDir}/current
+
+      systemd-run --user ${lib.meta.getExe' pkgs.imagemagick "magick"} "$wallpaper" -resize 50% -blur 0x10 "${dataDir}/lockscreen.png"
+      systemd-run --user ${lib.meta.getExe config.programs.wallust.package} run --dynamic-threshold --skip-sequences "$wallpaper"
+    ''
+  }";
 in
 {
   services.wpaperd = {
     enable = true;
     settings = {
+      DP-1 = {
+        duration = "15m";
+        mode = "stretch";
+        path = "${config.home.homeDirectory}/sync/Redmage/Windows";
+        exec = postWallpaperScript; # Desktop Main monitor.
+      };
+      eDP-1 = {
+        duration = "15m";
+        mode = "stretch";
+        path = "${config.home.homeDirectory}/sync/Redmage/Windows";
+        exec = postWallpaperScript; # Laptop Monitor
+      };
+      # Other monitors.
       default = {
         duration = "15m";
         mode = "stretch";
         path = "${config.home.homeDirectory}/sync/Redmage/Windows";
-        exec =
-          let
-            magick = lib.meta.getExe' pkgs.imagemagick "magick";
-          in
-          "${pkgs.writeShellScript "post-wallpaper-change" # sh
-            ''
-              wallpaper=$2
-
-              set -e
-
-              ln -sfn "$wallpaper" ${dataDir}/current
-
-              systemd-run --user ${magick} "$wallpaper" -resize 50% -blur 0x10 "${dataDir}/lockscreen.png"
-              systemd-run --user ${lib.meta.getExe config.programs.wallust.package} run --dynamic-threshold --skip-sequences "$wallpaper"
-            ''
-          }";
       };
     };
   };
