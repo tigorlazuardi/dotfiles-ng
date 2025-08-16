d4f4b1aff58a03072416b860e99d6a02233c182b
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-16 21:49:07 +0700

    feat(wpaperd): modularize the exec script config so other modules can reference it

--- environments/window-manager/home-manager/wpaperd.nix
+++ environments/window-manager/home-manager/wpaperd.nix
@@ -6,46 +6,50 @@
 }:
 let
   dataDir = "${config.xdg.dataHome}/wallpapers";
-  postWallpaperScript = "${pkgs.writeShellScript "post-wallpaper-change" # sh
-    ''
-      wallpaper=$2
-
-      set -e
-
-      ln -sfn "$wallpaper" ${dataDir}/current
-
-      systemd-run --user ${lib.meta.getExe' pkgs.imagemagick "magick"} "$wallpaper" -resize 50% -blur 0x10 "${dataDir}/lockscreen.png"
-      systemd-run --user ${lib.meta.getExe config.programs.wallust.package} run --dynamic-threshold --skip-sequences "$wallpaper"
-    ''
-  }";
 in
 {
-  services.wpaperd = {
-    enable = true;
-    settings = {
-      DP-1 = {
-        duration = "15m";
-        mode = "stretch";
-        path = "${config.home.homeDirectory}/sync/Redmage/Windows";
-        exec = postWallpaperScript; # Desktop Main monitor.
-      };
-      eDP-1 = {
-        duration = "15m";
-        mode = "stretch";
-        path = "${config.home.homeDirectory}/sync/Redmage/Windows";
-        exec = postWallpaperScript; # Laptop Monitor
-      };
-      # Other monitors.
-      default = {
-        duration = "15m";
-        mode = "stretch";
-        path = "${config.home.homeDirectory}/sync/Redmage/Windows";
+  options.services.wpaperd.execScript =
+    with lib;
+    mkOption {
+      type = types.lines;
+      default = "";
+    };
+  config = {
+    services.wpaperd = {
+      enable = true;
+      execScript =
+        lib.mkBefore # sh
+          ''
+            monitor=$1
+            wallpaper=$2
+            set -e
+            ln -sfn "$wallpaper" ${dataDir}/current
+          '';
+      settings = {
+        DP-1 = {
+          duration = "15m";
+          mode = "stretch";
+          path = "${config.home.homeDirectory}/sync/Redmage/Windows";
+          exec = "${pkgs.writeShellScript "wpaperd-exec-script" config.services.wpaperd.execScript}"; # Desktop Main monitor.
+        };
+        eDP-1 = {
+          duration = "15m";
+          mode = "stretch";
+          path = "${config.home.homeDirectory}/sync/Redmage/Windows";
+          exec = "${pkgs.writeShellScript "wpaperd-exec-script" config.services.wpaperd.execScript}"; # Desktop Main monitor.
+        };
+        # Other monitors.
+        default = {
+          duration = "15m";
+          mode = "stretch";
+          path = "${config.home.homeDirectory}/sync/Redmage/Windows";
+        };
       };
     };
+    systemd.user.services.wpaperd.Service.ExecStartPre =
+      pkgs.writeShellScript "create-wpaper-dir" # sh
+        ''
+          mkdir -p ${dataDir}
+        '';
   };
-  systemd.user.services.wpaperd.Service.ExecStartPre =
-    pkgs.writeShellScript "create-wpaper-dir" # sh
-      ''
-        mkdir -p ${dataDir}
-      '';
 }
