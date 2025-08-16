07399d9cc48886bbcd985a9e42d26f9e03c6b223
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-16 21:49:55 +0700

    feat(avizo): add wallust template for avizo

--- environments/hyprland/home-manager/avizo.nix
+++ environments/hyprland/home-manager/avizo.nix
@@ -1,10 +1,19 @@
 # Avizo is a volume/brightness indicator service
+{ config, pkgs, ... }:
 {
-  services.avizo = {
-    enable = true;
-    settings.default = {
-      time = 2;
+  services.avizo.enable = true;
+  stylix.targets.avizo.enable = false;
+  xdg.configFile."wallust/templates/avizo.ini".source = (pkgs.formats.ini { }).generate "avizo.ini" {
+    default = {
+      background = "rgba({{ background | rgb }}, {{ alpha_dec }})";
+      bar-bg-color = "rgba({{ color0 | rgb }}, {{ alpha_dec }})";
+      bar-fg-color = "rgba({{ foreground | rgb }}, {{ alpha_dec }})";
+      time = 1;
     };
+  };
+  programs.wallust.settings.templates.avizo = {
+    src = "avizo.ini";
+    dst = "${config.xdg.configHome}/avizo/config.ini";
   };
 
   wayland.windowManager.hyprland.settings.bind = [
