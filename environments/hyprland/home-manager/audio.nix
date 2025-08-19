ccf620a51d08a04c17d120e0de21dc505de50ee9
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-19 13:20:51 +0700

    feat(hyprland): add audio and brightness controls to Hyprland SwayOSD

--- environments/hyprland/home-manager/audio.nix
+++ environments/hyprland/home-manager/audio.nix
@@ -18,10 +18,6 @@
       playerctl = lib.meta.getExe pkgs.playerctl;
     in
     [
-      ", XF86AudioRaisevolume, exec, swayosd-client --output-volume raise"
-      ", XF86AudioLowervolume, exec, swayosd-client --output-volume lower"
-      ", XF86AudioMute, exec, swayosd-client --output-volume mute-toggle"
-      ", XF86AudioMicMute, exec, swayosd-client --input-volume mute-toggle"
       ", XF86AudioPlay, exec, ${playerctl} play-pause"
       ", XF86AudioPause, exec, ${playerctl} play-pause"
       ", XF86AudioNext, exec, ${playerctl} next"
--- environments/hyprland/home-manager/swayosd.nix
+++ environments/hyprland/home-manager/swayosd.nix
@@ -2,4 +2,13 @@
   imports = [
     ../../window-manager/home-manager/swayosd.nix
   ];
+
+  wayland.windowManager.hyprland.settings.binde = [
+    ", XF86AudioRaisevolume, exec, swayosd-client --output-volume raise"
+    ", XF86AudioLowervolume, exec, swayosd-client --output-volume lower"
+    ", XF86AudioMute, exec, swayosd-client --output-volume mute-toggle"
+    ", XF86AudioMicMute, exec, swayosd-client --input-volume mute-toggle"
+    ", XF86MonBrightnessUp, exec, swayosd-client --brightness raise"
+    ", XF86MonBrightnessDown, exec, swayosd-client --brightness lower"
+  ];
 }
