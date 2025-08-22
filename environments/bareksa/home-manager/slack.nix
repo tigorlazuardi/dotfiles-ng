a2d3a183008329ca5f5c8dee609e422dd7ff970d
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-22 10:16:54 +0700

    feat(slack): now uses systemd instead of xdg.autostart

--- environments/bareksa/home-manager/slack.nix
+++ environments/bareksa/home-manager/slack.nix
@@ -1,39 +1,36 @@
-{ pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  ...
+}:
 {
   home.packages = with pkgs; [
     slack
   ];
 
-  # Register a slack desktop entry to an autostart. But since slack requires internet connection
-  # to slack.com, we need to ensure that the network is available and able
-  # to connect to slack.com before starting slack.
-  #
-  # Hence we will use netcat to check if slack.com is reachable on port 443
-  # before starting slack, then uses sed to modify the original slack.desktop file
-  # and replace the Exec line with the new command that checks the network.
-  #
-  # Slack should only be started on office hours.
-  xdg.autostart.entries = [
-    (
-      let
-        script = pkgs.writeShellScriptBin "slack-autostart" ''
-          weekdayNum=$(date +%w) # 1-5 for Mon-Fri, 0 for Sun, 6 for Sat
-          hour=$(date +%H)
+  systemd.user.services.slack = {
+    Unit = rec {
+      Description = "Slack autostart service";
+      After = [ config.wayland.systemd.target ];
+      PartOf = After;
+      Requisite = After;
+    };
+    Service.ExecStart = pkgs.writeShellScript "slack-autostart" ''
+      weekdayNum=$(date +%w) # 1-5 for Mon-Fri, 0 for Sun, 6 for Sat
+      hour=$(date +%H)
 
-          # Only run during office hours: Mon-Fri (1-5), 8 AM - 5 PM (08-17)
-          if [ "$weekdayNum" -ge 1 ] && [ "$weekdayNum" -le 5 ] && [ "$hour" -ge 8 ] && [ "$hour" -lt 17 ]; then
-            until ${pkgs.netcat}/bin/nc -z slack.com 443 > /dev/null; do
-              sleep 0.1
-            done
-            ${pkgs.slack}/bin/slack "$@"
-          fi
-        '';
-        inherit (lib.meta) getExe;
-      in
-      (pkgs.runCommand "slack.desktop" { }) # sh
-        ''
-          sed -e 's#Exec=.*#Exec=${getExe script} -s %U#' ${pkgs.slack}/share/applications/slack.desktop > $out
-        ''
-    )
-  ];
+      # Only run during office hours: Mon-Fri (1-5), 8 AM - 5 PM (08-17)
+      if [ "$weekdayNum" -ge 1 ] && [ "$weekdayNum" -le 5 ] && [ "$hour" -ge 8 ] && [ "$hour" -lt 17 ]; then
+        # To ensure we do not start with error internet page on slack app
+        # and thus requires restart in the app,
+        # we will wait until slack.com is reachable before opening slack
+        # itself.
+        until ${pkgs.netcat}/bin/nc -z slack.com 443 > /dev/null; do
+          sleep 0.1
+        done
+        ${pkgs.slack}/bin/slack
+      fi
+    '';
+    Install.WantedBy = [ config.wayland.systemd.target ];
+  };
 }
