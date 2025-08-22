0a43ea922e94501fedcbd0dac047825fc1665ec9
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-22 10:07:19 +0700

    fix(hyprland): fix workspace rules and add noinitialfocus for specific apps

--- environments/hyprland/home-manager/window_rules.nix
+++ environments/hyprland/home-manager/window_rules.nix
@@ -1,7 +1,10 @@
 {
   wayland.windowManager.hyprland.settings.windowrule = [
-    "workspace:5, class:wasistlos"
-    "workspace:6, class:Slack"
-    "workspace:7, class:vesktop"
+    "workspace 5, class:wasistlos"
+    "noinitialfocus, class:wasistlos"
+    "workspace 6, class:Slack"
+    "noinitialfocus, class:Slack"
+    "workspace 7, class:vesktop"
+    "noinitialfocus, class:vesktop"
   ];
 }
