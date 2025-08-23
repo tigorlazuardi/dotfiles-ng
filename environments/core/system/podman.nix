84ebfe72467456d82d6264e1f2b685fe03aabacb
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-23 22:40:39 +0700

    fix(podman): correct autoupdate label key and enabled daily auto-update timer

--- environments/core/system/podman.nix
+++ environments/core/system/podman.nix
@@ -53,7 +53,7 @@
               networks = mkDefault [ "podman" ];
               autoStart = mkDefault (!config.socketActivation.enable);
               labels = mkDefault {
-                "io.container.autoupdate" = "registry";
+                "io.containers.autoupdate" = "registry";
               };
               extraOptions = optional (config.ip != null) "--ip=${config.ip}";
             };
@@ -73,76 +73,89 @@
       cfg = config.virtualisation.oci-containers.containers;
       socketActivatedContainers = filterAttrs (_: c: c.socketActivation.enable) cfg;
     in
-    {
-      systemd.socketActivations = mapAttrs' (
-        name: value:
-        (nameValuePair "podman-${name}" {
-          host = mkIf (assertMsg (value.ip != null)
-            "virtualisation.oci-containers.containers.${name}.ip must not be null to fulfill the conditions to have socketActivation enabled"
-          ) value.ip;
-          port = mkIf (assertMsg (value.httpPort != null)
-            "virtualisation.oci-containers.containers.${name}.httpPort must not be null to fulfill the conditions to have socketActivation enabled"
-          ) value.httpPort;
-          idleTimeout = value.socketActivation.idleTimeout;
-        })
-      ) socketActivatedContainers;
-      virtualisation.podman = {
-        autoPrune.enable = true;
-        defaultNetwork.settings.dns_enabled = true;
-      };
-      networking.firewall.interfaces."podman[0-9]+" = {
-        allowedUDPPorts = [ 53 ]; # this needs to be there so that containers can look eachother's names up over DNS
-      };
-      environment.systemPackages = [
-        (pkgs.writeShellScriptBin "pod-ips" ''
-          sudo podman inspect --format '{{.Name}} - {{.NetworkSettings.IPAddress}}' $(sudo podman ps -q) | sort -t . -k 3,4
-        '')
-      ];
-      security.sudo.extraRules = [
-        {
-          users = [ user.name ];
-          commands = [
-            {
-              command = "/run/current-system/sw/bin/podman";
-              options = [
-                "SETENV"
-                "NOPASSWD"
-              ];
-            }
-          ];
-        }
-      ];
-      # Ensure images are pulled for socket activated containers.
-      systemd.services = mapAttrs' (
-        name: value:
-        nameValuePair "podman-${name}-ensure-image" {
-          script =
-            let
-              inherit (pkgs) podman;
-            in
-            # sh
-            ''
-              ${pkgs.waitport}/bin/waitport 600 docker.io 443
-              set -e
-              if ! ${podman}/bin/podman image exists ${value.image}; then
-                ${podman}/bin/podman pull ${value.image};
-              fi
-            '';
-          unitConfig = {
-            StartLimitIntervalSec = "1h";
-            StartLimitBurst = 5;
+    lib.mkMerge [
+      {
+        systemd.socketActivations = mapAttrs' (
+          name: value:
+          (nameValuePair "podman-${name}" {
+            host = mkIf (assertMsg (value.ip != null)
+              "virtualisation.oci-containers.containers.${name}.ip must not be null to fulfill the conditions to have socketActivation enabled"
+            ) value.ip;
+            port = mkIf (assertMsg (value.httpPort != null)
+              "virtualisation.oci-containers.containers.${name}.httpPort must not be null to fulfill the conditions to have socketActivation enabled"
+            ) value.httpPort;
+            idleTimeout = value.socketActivation.idleTimeout;
+          })
+        ) socketActivatedContainers;
+        virtualisation.podman = {
+          autoPrune.enable = true;
+          defaultNetwork.settings.dns_enabled = true;
+        };
+        networking.firewall.interfaces."podman[0-9]+" = {
+          allowedUDPPorts = [ 53 ]; # this needs to be there so that containers can look eachother's names up over DNS
+        };
+        environment.systemPackages = [
+          (pkgs.writeShellScriptBin "pod-ips" ''
+            sudo podman inspect --format '{{.Name}} - {{.NetworkSettings.IPAddress}}' $(sudo podman ps -q) | sort -t . -k 3,4
+          '')
+        ];
+        security.sudo.extraRules = [
+          {
+            users = [ user.name ];
+            commands = [
+              {
+                command = "/run/current-system/sw/bin/podman";
+                options = [
+                  "SETENV"
+                  "NOPASSWD"
+                ];
+              }
+            ];
+          }
+        ];
+        # Ensure images are pulled for socket activated containers.
+        systemd.services = mapAttrs' (
+          name: value:
+          nameValuePair "podman-${name}-ensure-image" {
+            script =
+              let
+                inherit (pkgs) podman;
+              in
+              # sh
+              ''
+                ${pkgs.waitport}/bin/waitport 600 docker.io 443
+                set -e
+                if ! ${podman}/bin/podman image exists ${value.image}; then
+                  ${podman}/bin/podman pull ${value.image};
+                fi
+              '';
+            unitConfig = {
+              StartLimitIntervalSec = "1h";
+              StartLimitBurst = 5;
+            };
+            serviceConfig = {
+              Type = "simple";
+              RemainAfterExit = true;
+              Restart = "on-failure";
+              RestartSec = 5;
+              RestartSteps = 5;
+              RestartMaxDelaySec = 30;
+            };
+            after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
+          }
+        ) socketActivatedContainers;
+      }
+      {
+        systemd = {
+          timers.podman-auto-update = {
+            description = "Timer to auto update podman containers";
+            timerConfig = {
+              OnCalendar = "daily";
+            };
+            wantedBy = [ "timers.target" ];
           };
-          serviceConfig = {
-            Type = "simple";
-            RemainAfterExit = true;
-            Restart = "on-failure";
-            RestartSec = 5;
-            RestartSteps = 5;
-            RestartMaxDelaySec = 30;
-          };
-          after = [ "network.target" ];
-          wantedBy = [ "multi-user.target" ];
-        }
-      ) socketActivatedContainers;
-    };
+        };
+      }
+    ];
 }
