81ddecc90255a571b77b3f8e3d7c0603a4d64908
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-25 22:32:04 +0700

    feat(hyprland): sherlock select window now only use SUPER key instead of SUPER+W

--- environments/hyprland/home-manager/sherlock.nix
+++ environments/hyprland/home-manager/sherlock.nix
@@ -4,44 +4,44 @@
   ...
 }:
 let
-  listHyprlandWindowJS =
-    pkgs.writeText "list-hyprland-windows.mjs" # javascript
-      ''
-        import { spawnSync } from "node:child_process";
+  listHyprlandWindowJS = pkgs.writers.writeJS "list-hyprland-windows.mjs" { } ''
+    import { spawnSync } from "node:child_process";
 
-        const result = spawnSync("hyprctl", ["clients", "-j"]);
-        if (result.error) {
-          console.error("Error executing hyprctl clients -j:", result.error);
-          process.exit(1);
-        }
-        const output = JSON.parse(result.stdout.toString());
-        const iconMappings = {
-          "vivaldi-stable": "vivaldi",
-          footclient: "foot",
-          wasistlos: "com.github.xeco23.WasIstLos",
-          spotify: "spotify-client",
-        };
-        const elements = output.map((client) => ({
-          title: client.title,
-          icon: (() => {
-            const clientClass = client.class.toLowerCase();
-            return iconMappings[clientClass] || clientClass;
-          })(),
-          icon_size: 64,
-          description: `Workspace ''${client.workspace.name} · ''${client.class}`,
-          field: "exec",
-          method: "print",
-          hidden: {
-            exec: `hyprctl dispatch focuswindow address:''${client.address}`,
-          },
-        }));
-        console.log(
-          JSON.stringify({
-            settings: [],
-            elements,
-          }),
-        );
-      '';
+    const result = spawnSync("hyprctl", ["clients", "-j"]);
+    if (result.error) {
+      console.error("Error executing hyprctl clients -j:", result.error);
+      process.exit(1);
+    }
+    const output = JSON.parse(result.stdout.toString());
+    const iconMappings = {
+      "vivaldi-stable": "vivaldi",
+      footclient: "foot",
+      wasistlos: "com.github.xeco23.WasIstLos",
+      spotify: "spotify-client",
+    };
+    const elements = output.map((client) => ({
+      title: client.title,
+      icon: (() => {
+        const clientClass = client.class.toLowerCase();
+        return iconMappings[clientClass] || clientClass;
+      })(),
+      icon_size: 64,
+      description: `Workspace ''${client.workspace.name} · ''${client.class}`,
+      field: "exec",
+      method: "print",
+      hidden: {
+        exec: `hyprctl dispatch focuswindow address:''${client.address}`,
+      },
+    }));
+    const selectedResult = spawnSync("sherlock", [], {
+      input: JSON.stringify({ elements }),
+    });
+    const cmd = selectedResult.stdout.toString().trim();
+    if (!cmd) {
+      process.exit(0);
+    }
+    spawnSync("bash", ["-c", cmd]);
+  '';
 in
 {
   imports = [
@@ -50,12 +50,6 @@ in
 
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "$mod, W, exec, ${pkgs.writeShellScript "select-hyprland-window-sherlock" ''
-        selected=$(${pkgs.nodejs}/bin/node ${listHyprlandWindowJS} | ${config.programs.sherlock.package}/bin/sherlock)
-        if [ -n "$selected" ]; then
-          eval "$selected"
-        fi
-      ''}"
       "$mod, BackSpace, exec, systemd-run --user sherlock --sub-menu pm"
       "$mod, A, exec, systemd-run --user sherlock-select-audio"
       "$mod, C, exec, systemd-run --user sherlock-clipboard"
@@ -64,6 +58,9 @@ in
       "$mod, N, exec, systemd-run --user sherlock-zoxide neovide"
       "$mod, M, exec, systemd-run --user sherlock-zoxide nemo"
       "$mod, D, exec, systemd-run --user sherlock"
+    ];
+    bindr = [
+      "SUPER, Super_L, exec, systemd-run --user ${listHyprlandWindowJS}"
     ];
     layerrule = [
       "blur, sherlock"
