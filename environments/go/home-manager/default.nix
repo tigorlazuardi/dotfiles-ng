76f5d407a7cf5f3f4a66d188b1b8f4d872f6e333
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-25 15:40:57 +0700

    feat(go): added go packages to environment

--- environments/go/home-manager/default.nix
+++ environments/go/home-manager/default.nix
@@ -1,21 +1,24 @@
 { config, pkgs, ... }:
+let
+  goPackages = with pkgs; [
+    go
+    wgo
+    gotools
+    gomodifytags
+    gotests
+    iferr
+    gopls
+    gofumpt
+    impl
+    golangci-lint
+    delve
+  ];
+in
 {
   programs.go.enable = true;
+  home.packages = goPackages;
   home.file."go/bin" =
     let
-      goPackages = with pkgs; [
-        go
-        wgo
-        gotools
-        gomodifytags
-        gotests
-        iferr
-        gopls
-        gofumpt
-        impl
-        golangci-lint
-        delve
-      ];
       merged = pkgs.symlinkJoin {
         name = "home-go-bin";
         paths = goPackages;
