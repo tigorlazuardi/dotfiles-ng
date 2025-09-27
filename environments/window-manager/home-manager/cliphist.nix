96638569d55eb90accec78c13ca826a93e58140c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-09-27 16:04:15 +0700

    feat(cliphist): reduced max items to 200 and max dedupe search to 10

--- environments/window-manager/home-manager/cliphist.nix
+++ environments/window-manager/home-manager/cliphist.nix
@@ -1,3 +1,12 @@
 {
-  services.cliphist.enable = true;
+  services.cliphist = {
+    enable = true;
+    extraOptions = [
+      # The default options store huge amount of history. Querying the history is slow. So we limit it.
+      "-max-items"
+      "200"
+      "max-dedupe-search"
+      "10"
+    ];
+  };
 }
