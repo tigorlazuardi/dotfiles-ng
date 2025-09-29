cd7a28e99fc5ffb2004d62223dea43e124ab938e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-09-29 20:40:42 +0700

    feat(slack): start time now uses systemd timer instead of date check

--- environments/bareksa/home-manager/slack.nix
+++ environments/bareksa/home-manager/slack.nix
@@ -8,6 +8,16 @@
     slack
   ];
 
+  systemd.user.timers.slack = {
+    Unit = {
+      Description = "Timer to start Slack during office hours";
+    };
+    Timer = {
+      OnCalendar = "Mon..Fri *-*-* 08..18:*:*"; # Every day at 8 AM to 6 PM.
+    };
+    Install.WantedBy = [ "timers.target" ];
+  };
+
   systemd.user.services.slack = {
     Unit = rec {
       Description = "Slack autostart service";
@@ -17,22 +27,12 @@
     };
     Service = {
       ExecStart = pkgs.writeShellScript "slack-autostart" ''
-        weekdayNum=$(date +%w) # 1-5 for Mon-Fri, 0 for Sun, 6 for Sat
-        hour=$(date +%H)
-
-        # Only run during office hours: Mon-Fri (1-5), 8 AM - 5 PM (08-17)
-        if [ "$weekdayNum" -ge 1 ] && [ "$weekdayNum" -le 5 ] && [ "$hour" -ge 8 ] && [ "$hour" -lt 17 ]; then
-          # To ensure we do not start with error internet page on slack app
-          # and thus requires restart in the app,
-          # we will wait until slack.com is reachable before opening slack
-          # itself.
-          until ${pkgs.netcat}/bin/nc -z slack.com 443 > /dev/null; do
-            sleep 0.1
-          done
-          ${pkgs.slack}/bin/slack --enable-features=UseOzonePlatform --ozone-platform=wayland
-        fi
+        until ${pkgs.netcat}/bin/nc -z slack.com 443 > /dev/null; do
+          sleep 0.1
+        done
+        ${pkgs.slack}/bin/slack --enable-features=UseOzonePlatform --ozone-platform=wayland
       '';
+      RemainAfterExit = true; # So the timer above doesn't auto-restart this.
     };
-    Install.WantedBy = [ config.wayland.systemd.target ];
   };
 }
