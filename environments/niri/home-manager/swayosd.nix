adccb2d16f0ff195f860429c8099d5e7bc6c6d1f
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-09-29 08:42:56 +0700

    fix(niri): swayosd-client typos

--- environments/niri/home-manager/swayosd.nix
+++ environments/niri/home-manager/swayosd.nix
@@ -7,19 +7,19 @@
   programs.niri.settings.binds = with config.lib.niri.actions; {
     "XF86AudioRaisevolume" = {
       allow-when-locked = true;
-      action = spawn "swaysosd-client" "--output-volume" "raise";
+      action = spawn "swayosd-client" "--output-volume" "raise";
     };
     "XF86AudioLowervolume" = {
       allow-when-locked = true;
-      action = spawn "swaysosd-client" "--output-volume" "lower";
+      action = spawn "swayosd-client" "--output-volume" "lower";
     };
     "XF86AudioMute" = {
       allow-when-locked = true;
-      action = spawn "swaysosd-client" "--output-volume" "mute-toggle";
+      action = spawn "swayosd-client" "--output-volume" "mute-toggle";
     };
     "XF86AudioMicMute" = {
       allow-when-locked = true;
-      action = spawn "swayosd-client" "--input-volume" "mute-toggle";
+      action = spawn "swaosd-client" "--input-volume" "mute-toggle";
     };
     "XF86MonBrightnessUp" = {
       allow-when-locked = true;
