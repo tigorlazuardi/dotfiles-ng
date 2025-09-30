46e759f887ff192ad7a8768b9a9cd63153c69864
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-09-30 15:02:57 +0700

    feat(fort): switch to niri for display management

---
+++ machines/fort/home-manager/niri.nix
@@ -0,0 +1,10 @@
+{
+  programs.niri.settings = {
+    outputs = {
+      eDP-1 = {
+        scale = 1.0;
+      };
+    };
+    input.keyboard.xkb.options = "ctrl:nocaps,shift:both_capslock_cancel";
+  };
+}
