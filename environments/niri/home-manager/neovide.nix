ea41f1eb7899caf49050b44ba1863118dc411e41
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-10-03 20:41:59 +0700

    feat(niri): neovide window rules to add shadow

--- environments/niri/home-manager/neovide.nix
+++ environments/niri/home-manager/neovide.nix
@@ -3,6 +3,17 @@
     {
       matches = [ { app-id = "neovide"; } ];
       open-maximized = true;
+      shadow = {
+        enable = true;
+        color = "#00000033";
+        draw-behind-window = true;
+        softness = 60;
+        spread = 30;
+        offset = {
+          x = 0;
+          y = 0;
+        };
+      };
     }
   ];
 }
