d18c530a0fb681ebfe7a9392dc1f9fe8645687b3
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-10-13 09:47:29 +0700

    feat(kdeconnect): disable kdeconnect autostart

--- environments/window-manager/home-manager/kdeconnect.nix
+++ environments/window-manager/home-manager/kdeconnect.nix
@@ -1,6 +1,8 @@
+{ lib, ... }:
 {
   services.kdeconnect = {
     enable = true;
     indicator = true;
   };
+  systemd.user.services.kdeconnect.Install.WantedBy = lib.mkForce [ ]; # Disable kdeconnect autostart. Let kdeconnect-indicator handle it.
 }
