549c9aa158d0995cd6212e296537947da22bf36c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-10-20 20:45:08 +0700

    feat(flameshot): uses home manager module

--- environments/desktop/home-manager/optional/flameshot.nix
+++ environments/desktop/home-manager/optional/flameshot.nix
@@ -1,29 +1,23 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    flameshot
     grim
   ];
 
-  xdg.configFile."flameshot/flameshot.ini".source = (pkgs.formats.ini { }).generate "flameshot.ini" {
-    General = {
-      contrastOpacity = 188;
-      saveAsFileExtension = "png";
-      savePath = "${config.home.homeDirectory}/Pictures/Screenshots";
-      useGrimAdapter = true;
+  services.flameshot = {
+    enable = true;
+    settings = {
+      General = {
+        contrastOpacity = 188;
+        saveAsFileExtension = ".png";
+        savePath = "${config.home.homeDirectory}/Pictures/Screenshots";
+        useGrimAdapter = true;
+        filenamePattern = "=%d-%m-%Y_%T";
+        showStartupLaunchMessage = false;
+      };
     };
   };
 
-  systemd.user.services.flameshot = {
-    Unit = {
-      Description = "Flameshot screenshot tool";
-      After = [ config.wayland.systemd.target ];
-      PartOf = [ config.wayland.systemd.target ];
-    };
-    Service = {
-      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${config.home.homeDirectory}/Pictures/Screenshots";
-      ExecStart = "${pkgs.flameshot}/bin/flameshot";
-    };
-    Install.WantedBy = [ config.wayland.systemd.target ];
-  };
+  systemd.user.services.flameshot.Service.ExecStartPre =
+    "${pkgs.coreutils}/bin/mkdir -p ${config.home.homeDirectory}/Pictures/Screenshots";
 }
