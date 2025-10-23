97f7cde136cdb0d5aa7958566783e3b06e57e4be
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-10-23 16:19:07 +0700

    feat(tlp): set CPU_MAX_PERF_ON_BAT to 100

--- machines/fort/system/default.nix
+++ machines/fort/system/default.nix
@@ -62,7 +62,7 @@
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 70;
+      CPU_MAX_PERF_ON_BAT = 100;
 
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
