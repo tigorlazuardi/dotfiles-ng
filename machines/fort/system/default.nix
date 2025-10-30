e662aed3ea735bd2c228c316b3b2fbc38ceb07fb
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-10-30 13:41:55 +0700

    feat(fort): update TLP energy performance policy for battery

--- machines/fort/system/default.nix
+++ machines/fort/system/default.nix
@@ -56,7 +56,7 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
       CPU_MIN_PERF_ON_AC = 0;
