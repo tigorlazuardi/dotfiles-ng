f4c6a61cbfb0b51b3000b224df8adde39ddcca86
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-11-17 11:11:09 +0700

    feat(n8n): move to use environment options

--- environments/homeserver/system/n8n.nix
+++ environments/homeserver/system/n8n.nix
@@ -5,19 +5,18 @@ in
 {
   services.n8n = {
     enable = true;
-    webhookUrl = "https://${domain}";
-  };
-  systemd.services.n8n.environment = {
-    # We let Tinyauth handles the authentications.
-    N8N_USER_MANAGEMENT_JWT_SECRET = "FUCK-SAML-LOGINS";
-    N8N_USER_MANAGEMENT_JWT_DURATION_HOURS = toString (24 * 365 * 365 * 365);
-    N8N_USER_MANAGEMENT_JWT_REFRESH_TIMEOUT_HOURS = toString (24 * 365 * 365 * 365);
-    N8N_RUNNERS_ENABLED = "true";
-    GENERIC_TIMEZONE = "Asia/Jakarta";
+    environment = {
+      WEBHOOK_URL = "https://${domain}";
+      N8N_USER_MANAGEMENT_JWT_SECRET = "FUCK-SAML-LOGINS";
+      N8N_USER_MANAGEMENT_JWT_DURATION_HOURS = toString (24 * 365 * 365 * 365);
+      N8N_USER_MANAGEMENT_JWT_REFRESH_TIMEOUT_HOURS = toString (24 * 365 * 365 * 365);
+      N8N_RUNNERS_ENABLED = "true";
+      GENERIC_TIMEZONE = "Asia/Jakarta";
+    };
   };
   services.nginx.virtualHosts."${domain}" = {
     forceSSL = true;
     tinyauth.enable = true;
-    locations."/".proxyPass = "http://localhost:${toString config.services.n8n.settings.port}";
+    locations."/".proxyPass = "http://localhost:${config.services.n8n.environment.N8N_PORT}";
   };
 }
