81bb805b7077f390c2117299563d313d06f8ef0d
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-11-23 22:28:53 +0700

    feat(steam): enable more features

--- environments/game/system/steam.nix
+++ environments/game/system/steam.nix
@@ -1,7 +1,17 @@
+{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
   };
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+  hardware.xone.enable = true; # support for the xbox controller USB dongle
+  # environment.systemPackages = with pkgs; [
+  #   steam-run
+  # ];
 }
