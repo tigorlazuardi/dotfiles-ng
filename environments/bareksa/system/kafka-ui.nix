95cf8864689e6cc90c0497c21f373d4f361d383d
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-11-23 20:14:32 +0700

    feat(kafka-ui): migrate to Kouncil from Kafka-UI

--- environments/bareksa/system/kafka-ui.nix
+++ environments/bareksa/system/kafka-ui.nix
@@ -4,41 +4,59 @@
   ...
 }:
 let
-  settings = {
-    kafka = {
+  settings = (pkgs.formats.yaml { }).generate "kouncil.yaml" {
+    kouncil = {
       clusters = [
         {
           name = "Bareksa Development";
-          bootstrapServers = "kafka.dev.bareksa.local:9093,kafka.dev.bareksa.local:9094,kafka.dev.bareksa.local:9095";
+          brokers = [
+            {
+              host = "kafka.dev.bareksa.local";
+              port = 9093;
+            }
+            {
+              host = "kafka.dev.bareksa.local";
+              port = 9094;
+            }
+            {
+              host = "kafka.dev.bareksa.local";
+              port = 9095;
+            }
+          ];
         }
         {
           name = "Bareksa Production";
-          bootstrapServers = "192.168.50.102:9092,192.168.50.103:9092,192.168.50.104:9092";
-          readOnly = true;
+          brokers = [
+            {
+              host = "192.168.50.102";
+              port = 9092;
+            }
+            {
+              host = "192.168.50.103";
+              port = 9092;
+            }
+            {
+              host = "192.168.50.104";
+              port = 9092;
+            }
+          ];
         }
       ];
     };
   };
-  yaml = pkgs.formats.yaml { };
+
 in
 {
-  sops.templates."bareksa/kafka-ui/config.yaml" = {
-    file = yaml.generate "config.yaml" settings;
-    mode = "0444";
-  };
   virtualisation.oci-containers.containers.bareksa-kafka-ui = {
-    image = "docker.io/provectuslabs/kafka-ui:latest";
+    image = "docker.io/consdata/kouncil:latest";
     ip = "10.88.200.2";
     httpPort = 8080;
     volumes = [
-      "${config.sops.templates."bareksa/kafka-ui/config.yaml".path}:/config.yaml"
+      "${settings}:/config/kouncil.yaml"
     ];
     socketActivation = {
       enable = true;
-      idleTimeout = "15m";
-    };
-    environment = {
-      SPRING_CONFIG_ADDITIONAL-LOCATION = "/config.yaml";
+      idleTimeout = "30m";
     };
   };
   services.nginx.virtualHosts."kafka.bareksa.local".locations."/".proxyPass =
@@ -51,3 +69,42 @@ in
     192.168.50.104 kafka-host-3 kafka-cluster-jkt-3
   '';
 }
+# let
+#   settings = {
+#     kafka = {
+#       clusters = [
+#         {
+#           name = "Bareksa Development";
+#           bootstrapServers = "kafka.dev.bareksa.local:9093,kafka.dev.bareksa.local:9094,kafka.dev.bareksa.local:9095";
+#         }
+#         {
+#           name = "Bareksa Production";
+#           bootstrapServers = "192.168.50.102:9092,192.168.50.103:9092,192.168.50.104:9092";
+#           readOnly = true;
+#         }
+#       ];
+#     };
+#   };
+#   yaml = pkgs.formats.yaml { };
+# in
+# {
+#   sops.templates."bareksa/kafka-ui/config.yaml" = {
+#     file = yaml.generate "config.yaml" settings;
+#     mode = "0444";
+#   };
+#   virtualisation.oci-containers.containers.bareksa-kafka-ui = {
+#     image = "docker.io/provectuslabs/kafka-ui:latest";
+#     ip = "10.88.200.2";
+#     httpPort = 8080;
+#     volumes = [
+#       "${config.sops.templates."bareksa/kafka-ui/config.yaml".path}:/config.yaml"
+#     ];
+#     socketActivation = {
+#       enable = true;
+#       idleTimeout = "15m";
+#     };
+#     environment = {
+#       SPRING_CONFIG_ADDITIONAL-LOCATION = "/config.yaml";
+#     };
+#   };
+# }
