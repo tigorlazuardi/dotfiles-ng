a4e2f20b43e6049837412dce998bfed4c2502067
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-11-27 19:28:38 +0700

    feat(steam): removed unnecessary gamescope configuration

--- environments/game/system/steam.nix
+++ environments/game/system/steam.nix
@@ -4,12 +4,12 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    gamescopeSession.enable = true;
+    # gamescopeSession.enable = true;
   };
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
+  # programs.gamescope = {
+  #   enable = true;
+  #   capSysNice = true;
+  # };
   hardware.xone.enable = true; # support for the xbox controller USB dongle
   # environment.systemPackages = with pkgs; [
   #   steam-run
